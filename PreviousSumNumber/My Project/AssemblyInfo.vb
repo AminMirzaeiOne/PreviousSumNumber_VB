72fa4592f7363d8fd8b2ab3cd09e1016470dcd98
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-14 02:14:38 +0330

    - Update Assembly

--- PreviousSumNumber/My Project/AssemblyInfo.vb
+++ PreviousSumNumber/My Project/AssemblyInfo.vb
@@ -12,7 +12,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("Among the first projects of Amin Mirzaei after learning the basics of Visual Basic.NET")>
 <Assembly: AssemblyCompany("AminMirzaeiOne")>
 <Assembly: AssemblyProduct("PreviousSumNumber")>
-<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyCopyright("Copyright ©  2016")>
 <Assembly: AssemblyTrademark("AminMirzaeiOne")>
 
 <Assembly: ComVisible(False)>

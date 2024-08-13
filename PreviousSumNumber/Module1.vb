0c628a8e8428f0434df171a3202aa8ea3824e029
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-14 02:10:48 +0330

    Add project files.

---
+++ PreviousSumNumber/Module1.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim x, sum As Integer
+        System.Console.ForegroundColor = ConsoleColor.Cyan
+        System.Console.Write("Pls Enter Number: ")
+        System.Console.ForegroundColor = ConsoleColor.Yellow
+        x = Convert.ToInt32(Console.ReadLine())
+
+        sum = 0
+        For index = 1 To x - 1
+            sum += index
+        Next
+        System.Console.ForegroundColor = ConsoleColor.Blue
+        System.Console.Write("Sum Is: ")
+        System.Console.BackgroundColor = ConsoleColor.Blue
+        System.Console.ForegroundColor = ConsoleColor.White
+        System.Console.WriteLine(sum.ToString())
+        System.Console.BackgroundColor = ConsoleColor.Black
+        System.Console.ForegroundColor = ConsoleColor.Magenta
+        System.Console.WriteLine("-----------------------")
+        System.Console.ForegroundColor = ConsoleColor.Red
+        System.Console.WriteLine("Programmer : Amin Mirzaei - Github(UserName) : AminMirzaeiOne")
+        System.Console.ReadKey()
+
+    End Sub
+
+End Module
---
+++ PreviousSumNumber/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("PreviousSumNumber")>
+<Assembly: AssemblyDescription("Among the first projects of Amin Mirzaei after learning the basics of Visual Basic.NET")>
+<Assembly: AssemblyCompany("AminMirzaeiOne")>
+<Assembly: AssemblyProduct("PreviousSumNumber")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("AminMirzaeiOne")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("aa5d6a7b-64d3-41a3-803c-3c8c74fc0013")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
